14800ab799b44eec9759efcfb168b81fc73f7634
Дмитрий Жемчужнов <zhemchuzhnov.d@gmail.com>
2022-11-07 23:25:40 +0300

    Реализована отрисовка задач на странице

--- app.js
+++ app.js
@@ -1,16 +1,45 @@
 // globsl
 let todos = [];
 let users = [];
+const todoList = document.querySelector("#todo-list");
 
 // attach events
 document.addEventListener("DOMContentLoaded", initApp);
+
+// basic logic
+function getUserName(userId) {
+  const user = users.find((user) => user.id === userId);
+  return user.name;
+}
+
+function printTodo({ id, userId, title, completed }) {
+  const li = document.createElement("li");
+  li.classList.add("todo-item");
+  li.dataset.id = id;
+  li.innerHTML = `<span>${title} <i>by</i> <b>${getUserName(
+    userId
+  )}</b></span>`;
+
+  const status = document.createElement("input");
+  status.type = "checkbox";
+  status.checked = completed;
+
+  const close = document.createElement("span");
+  close.innerHTML = "&times;";
+  close.className = "close";
+
+  li.prepend(status);
+  li.append(close);
+
+  todoList.prepend(li);
+}
 
 // event logic
 function initApp() {
   Promise.all([getAllTodos(), getAllUsers()]).then((values) => {
     [todos, users] = values;
 
-    // Отправить в разметку
+    todos.forEach((todo) => printTodo(todo));
   });
 }
 
